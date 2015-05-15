0a8699b1483af4a93a2a5d65662042c4fffa5d31
Matthew Fortune <matthew.fortune@imgtec.com>
2015-05-15 16:55:10 +0100

    Add FPU/DSP/MSA/STACK context support

--- libgloss/mips/hal/include/mips/hal.h
+++ libgloss/mips/hal/include/mips/hal.h
@@ -34,6 +34,16 @@
 #include <mips/asm.h>
 #include <mips/m32c0.h>
 
+#if _MIPS_SIM == _ABIO32
+#define UHI_ABI 0
+#elif _MIPS_SIM == _ABIN32
+#define UHI_ABI 1
+#elif _MIPS_SIM == _ABI64
+#define UHI_ABI 2
+#else
+#error "UHI context structure is not defined for current ABI"
+#endif
+
 #define CTX_REG(REGNO)	((SZREG)*((REGNO)-1))
 
 #define CTX_AT		((SZREG)*0)
@@ -89,9 +99,128 @@
 #define CTX_BADPINSTR	(((SZREG)*35)+SZPTR+12)
 #define CTX_SIZE	(((SZREG)*35)+SZPTR+16)
 
+#define DSPCTX_DSPC ((SZREG)*2)
+#define DSPCTX_HI1  ((SZREG)*3)
+#define DSPCTX_HI2  ((SZREG)*4)
+#define DSPCTX_HI3  ((SZREG)*5)
+#define DSPCTX_LO1  ((SZREG)*6)
+#define DSPCTX_LO2  ((SZREG)*7)
+#define DSPCTX_LO3  ((SZREG)*8)
+#define DSPCTX_SIZE ((SZREG)*9)
+
+#define FP32CTX_CSR	((SZREG)*2)
+#define FP64CTX_CSR	((SZREG)*2)
+#define MSACTX_FCSR	((SZREG)*2)
+#define MSACTX_MSACSR	((SZREG)*3)
+
+#define MSACTX_0	((SZREG)*4)
+#define MSACTX_1	(MSACTX_0 + (1 * 16))
+#define MSACTX_2	(MSACTX_0 + (2 * 16))
+#define MSACTX_3	(MSACTX_0 + (3 * 16))
+#define MSACTX_4	(MSACTX_0 + (4 * 16))
+#define MSACTX_5	(MSACTX_0 + (5 * 16))
+#define MSACTX_6	(MSACTX_0 + (6 * 16))
+#define MSACTX_7	(MSACTX_0 + (7 * 16))
+#define MSACTX_8	(MSACTX_0 + (8 * 16))
+#define MSACTX_9	(MSACTX_0 + (9 * 16))
+#define MSACTX_10	(MSACTX_0 + (10 * 16))
+#define MSACTX_11	(MSACTX_0 + (11 * 16))
+#define MSACTX_12	(MSACTX_0 + (12 * 16))
+#define MSACTX_13	(MSACTX_0 + (13 * 16))
+#define MSACTX_14	(MSACTX_0 + (14 * 16))
+#define MSACTX_15	(MSACTX_0 + (15 * 16))
+#define MSACTX_16	(MSACTX_0 + (16 * 16))
+#define MSACTX_17	(MSACTX_0 + (17 * 16))
+#define MSACTX_18	(MSACTX_0 + (18 * 16))
+#define MSACTX_19	(MSACTX_0 + (19 * 16))
+#define MSACTX_20	(MSACTX_0 + (20 * 16))
+#define MSACTX_21	(MSACTX_0 + (21 * 16))
+#define MSACTX_22	(MSACTX_0 + (22 * 16))
+#define MSACTX_23	(MSACTX_0 + (23 * 16))
+#define MSACTX_24	(MSACTX_0 + (24 * 16))
+#define MSACTX_25	(MSACTX_0 + (25 * 16))
+#define MSACTX_26	(MSACTX_0 + (26 * 16))
+#define MSACTX_27	(MSACTX_0 + (27 * 16))
+#define MSACTX_28	(MSACTX_0 + (28 * 16))
+#define MSACTX_29	(MSACTX_0 + (29 * 16))
+#define MSACTX_30	(MSACTX_0 + (30 * 16))
+#define MSACTX_31	(MSACTX_0 + (31 * 16))
+#define MSACTX_SIZE	(MSACTX_0 + (32 * 16))
+
+#define FP32CTX_0	((SZREG)*4)
+#define FP32CTX_2	(FP32CTX_0 + (1 * 8))
+#define FP32CTX_4	(FP32CTX_0 + (2 * 8))
+#define FP32CTX_6	(FP32CTX_0 + (3 * 8))
+#define FP32CTX_8	(FP32CTX_0 + (4 * 8))
+#define FP32CTX_10	(FP32CTX_0 + (5 * 8))
+#define FP32CTX_12	(FP32CTX_0 + (6 * 8))
+#define FP32CTX_14	(FP32CTX_0 + (7 * 8))
+#define FP32CTX_16	(FP32CTX_0 + (8 * 8))
+#define FP32CTX_18	(FP32CTX_0 + (9 * 8))
+#define FP32CTX_20	(FP32CTX_0 + (10 * 8))
+#define FP32CTX_22	(FP32CTX_0 + (11 * 8))
+#define FP32CTX_24	(FP32CTX_0 + (12 * 8))
+#define FP32CTX_26	(FP32CTX_0 + (13 * 8))
+#define FP32CTX_28	(FP32CTX_0 + (14 * 8))
+#define FP32CTX_30	(FP32CTX_0 + (15 * 8))
+#define FP32CTX_SIZE	(FP32CTX_30 + (17 * 8))
+
+#define FP64CTX_0	((SZREG)*4)
+#define FP64CTX_2	(FP64CTX_0 + (1 * 8))
+#define FP64CTX_4	(FP64CTX_0 + (2 * 8))
+#define FP64CTX_6	(FP64CTX_0 + (3 * 8))
+#define FP64CTX_8	(FP64CTX_0 + (4 * 8))
+#define FP64CTX_10	(FP64CTX_0 + (5 * 8))
+#define FP64CTX_12	(FP64CTX_0 + (6 * 8))
+#define FP64CTX_14	(FP64CTX_0 + (7 * 8))
+#define FP64CTX_16	(FP64CTX_0 + (8 * 8))
+#define FP64CTX_18	(FP64CTX_0 + (9 * 8))
+#define FP64CTX_20	(FP64CTX_0 + (10 * 8))
+#define FP64CTX_22	(FP64CTX_0 + (11 * 8))
+#define FP64CTX_24	(FP64CTX_0 + (12 * 8))
+#define FP64CTX_26	(FP64CTX_0 + (13 * 8))
+#define FP64CTX_28	(FP64CTX_0 + (14 * 8))
+#define FP64CTX_30	(FP64CTX_0 + (15 * 8))
+#define FP64CTX_1	(FP64CTX_30 + (1 * 8))
+#define FP64CTX_3	(FP64CTX_30 + (2 * 8))
+#define FP64CTX_5	(FP64CTX_30 + (3 * 8))
+#define FP64CTX_7	(FP64CTX_30 + (4 * 8))
+#define FP64CTX_9	(FP64CTX_30 + (5 * 8))
+#define FP64CTX_11	(FP64CTX_30 + (6 * 8))
+#define FP64CTX_13	(FP64CTX_30 + (7 * 8))
+#define FP64CTX_15	(FP64CTX_30 + (8 * 8))
+#define FP64CTX_17	(FP64CTX_30 + (9 * 8))
+#define FP64CTX_19	(FP64CTX_30 + (10 * 8))
+#define FP64CTX_21	(FP64CTX_30 + (11 * 8))
+#define FP64CTX_23	(FP64CTX_30 + (12 * 8))
+#define FP64CTX_25	(FP64CTX_30 + (13 * 8))
+#define FP64CTX_27	(FP64CTX_30 + (14 * 8))
+#define FP64CTX_29	(FP64CTX_30 + (15 * 8))
+#define FP64CTX_31	(FP64CTX_30 + (16 * 8))
+#define FP64CTX_SIZE	(FP64CTX_31 + (17 * 8))
+
+#define FPCTX_SIZE()	(mips_getsr() & SR_FR ? FP64CTX_SIZE : FP32CTX_SIZE)
+
+#define LINKCTX_TYPE_MSA        0x004D5341
+#define LINKCTX_TYPE_FP32       0x46503332
+#define LINKCTX_TYPE_FP64       0x46503634
+#define LINKCTX_TYPE_FMSA       0x463D5341
+#define LINKCTX_TYPE_DSP        0x00445350
+#define LINKCTX_TYPE_STKSWP     0x53574150
+
+#define LINKCTX_ID      ((SZREG)*0)
+#define LINKCTX_NEXT    ((SZREG)*1)
+
+#define LINKCTX_TYPE(X) (((struct linkctx *)(X))->id)
+
+#define SWAPCTX_OLDSP   ((SZREG)*2)
+#define SWAPCTX_SIZE    ((SZREG)*3)
+
 #ifndef __ASSEMBLER__
 
 #include <stdint.h>
+
+struct linkctx;
 
 struct gpctx
 {
@@ -124,7 +253,7 @@ struct gpctx
   reg_t hi;
   reg_t lo;
   /* This field is for future extension */
-  void *link;
+  struct linkctx *link;
   /* Status at the point of the exception.  This may not be restored
      on return from exception if running under an RTOS */
   uint32_t status;
@@ -134,15 +263,103 @@ struct gpctx
   uint32_t badpinstr;
 };
 
-#if _MIPS_SIM == _ABIO32
-#define UHI_ABI 0
-#elif _MIPS_SIM == _ABIN32
-#define UHI_ABI 1
-#elif _MIPS_SIM == _ABI64
-#define UHI_ABI 2
+struct linkctx
+{
+  reg_t id;
+  struct linkctx *next;
+};
+
+struct swapctx
+{
+  struct linkctx link;
+  reg_t oldsp;
+};
+
+static inline void
+_linkctx_append (struct gpctx *gp, struct linkctx *nu)
+{
+  struct linkctx **ctx = (struct linkctx **)&gp->link;
+  while (*ctx)
+    ctx = &(*ctx)->next;
+  *ctx = nu;
+}
+
+struct dspctx
+{
+  struct linkctx link;
+  reg_t dspc;
+  reg_t hi[3];
+  reg_t lo[3];
+};
+
+struct fpctx
+{
+  struct linkctx link;
+  reg_t fcsr;
+  reg_t reserved;
+};
+
+typedef char _msareg[16] __attribute__ ((aligned(16)));
+
+struct msactx
+{
+  struct linkctx link;
+  reg_t fcsr;
+  reg_t msacsr;
+  union
+    {
+      _msareg w[32];
+      double d[64];
+      float s[128];
+    };
+};
+
+#define MSAMSACTX_D(CTX, N) (CTX)->w[(N)]
+#define MSACTX_DBL(CTX, N) (CTX)->d[(N) << 1]
+#ifdef __MIPSEL__
+#define MSACTX_SGL(CTX, N) (CTX)->s[(N) << 2]
 #else
-#error "UHI context structure is not defined for current ABI"
+#define MSACTX_SGL(CTX, N) (CTX)->s[((N) << 2) | 1]
 #endif
+
+struct fp32ctx
+{
+  struct fpctx fp;
+  union
+    {
+      double d[16];	/* even doubles */
+      float s[32];	/* even singles, padded */
+    };
+};
+
+#define FP32CTX_DBL(CTX, N) (CTX)->d[(N)]
+#ifdef __MIPSEL__
+#define FP32CTX_SGL(CTX, N) (CTX)->s[(N)]
+#else
+#define FP32CTX_SGL(CTX, N) (CTX)->s[(N) ^ 1]
+#endif
+
+struct fp64ctx
+{
+  struct fpctx fp;
+  union
+    {
+      double d[32];	/* even doubles, followed by odd doubles */
+      float s[64];	/* even singles, followed by odd singles, padded */
+    };
+};
+
+#define FP64CTX_DBL(CTX, N) (CTX)->d[((N) >> 1) + (((N) & 1) << 4)]
+#ifdef __MIPSEL__
+#define FP64CTX_SGL(CTX, N) (CTX)->s[((N) & ~1) + (((N) & 1) << 5)]
+#else
+#define FP64CTX_SGL(CTX, N) (CTX)->s[((N) | 1) + (((N) & 1) << 5)]
+#endif
+
+extern reg_t _fpctx_save (struct fpctx *ptr);
+extern reg_t _fpctx_load (struct fpctx *ptr);
+extern reg_t _msactx_save (struct msactx *ptr);
+extern reg_t _msactx_load (struct msactx *ptr);
 
 /* Fall back exception handlers:
    _mips_handle_exception - May be implemented by a user but is aliased
