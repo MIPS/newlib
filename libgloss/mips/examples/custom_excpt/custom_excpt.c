b9ab4c3da4e2e6ee24345999012cd6bad45d67e6
Matthew Fortune <matthew.fortune@imgtec.com>
2018-01-08 14:02:11 +0000

    Update to new custom exception handler example and use soft-float
    libgloss/
    	* mips/examples/custom_excpt/Makefile (CFLAGS): Add
    	-msoft-float.
    	(LDFLAGS): Likewise.
    	* mips/examples/custom_excpt/README.txt: Update description.
    	* mips/examples/custom_excpt/custom_excpt.c: Rewrite.
    	* mips/examples/fault_recovery/Makefile (CFLAGS): Add
    	-msoft-float.
    	(LDFLAGS): Likewise.
    	* mips/examples/isr_vector_space/Makefile (CFLAGS): Add
    	-msoft-float.
    	(LDFLAGS): Likewise.
    
    (cherry picked from commit b0b682d50f927000b2994699a920bd89f579de73)

--- libgloss/mips/examples/custom_excpt/custom_excpt.c
+++ libgloss/mips/examples/custom_excpt/custom_excpt.c
@@ -31,79 +31,78 @@
 #include <unistd.h>
 #include <mips/hal.h>
 #include <mips/cpu.h>
+
+/* Store a secret from register t0 */
+#define TRAPCODE1 14
+/* Retrieve a secret into register t0 */
+#define TRAPCODE2 15
+
+/* Count the traps we saw in the exception handler */
+volatile int num_traps = 0;
+
 int
-main ()
+main (void)
 {
-  /* Some pointer setup to access memory address 0*/
-  int b = -1;
-  int *a = 0;
-  /* Write to 0, triggering a TLB store error */
-  *a = 0;
-  /* Read from 0, triggering a TLB load error */
-  b = *a;
-  /* Return value should be 0 */
-  return b;
+  volatile register int secret __asm("$t0") = 0x1234;
+
+  /* Store a secret */
+  asm volatile ("teq $zero, $zero, %1\n"
+		::"r" (secret), "i" (TRAPCODE1));
+  secret = 0;
+  /* Retrieve the secret */
+  asm volatile ("teq $zero, $zero, %1\n"
+		:"=r" (secret): "i" (TRAPCODE2));
+
+  if (num_traps != 2)
+    {
+      printf ("2 traps expected to have been done. Failed.\n");
+      return 2;
+    }
+  else if (secret != 0x1234)
+    {
+      printf ("Expected secret to be 0x1234 but got 0x%x\n", secret);
+    }
+
+  printf ("Succeeded!\n");
+  return 0;
 }
 
-/*
- * This exception handler emulates register zero style handling but for memory,
- * i.e. writes to page zero are ignored and reads return a zero for select
- * instructions.
- */
 void
 _mips_handle_exception (struct gpctx *ctx, int exception)
 {
-  /*
-   * This example is designed to work only on MIPS32 and does not handle the
-   * case where a memory operation occurs in a branch delay slot.
-   */
+  /* The secret data we stash */
+  static int secret_store = 0;
+
   if ((mips32_get_c0 (C0_CAUSE) & CR_BD) == 0
-      && (ctx->epc & 0x1) == 0)
-    {
-      unsigned int fault_instruction = 0;
-      switch (exception)
-	{
-	case EXC_TLBS:
-	  fault_instruction = *((unsigned int *) (ctx->epc));
-	  if ((fault_instruction >> 26) == 0x2b)
-	    {
-	      /* Store instruction, ensure it went to zero. */
-	      unsigned int store_reg = (fault_instruction >> 21) & 0x1f;
-	      int16_t offset = fault_instruction & 0xffff;
-	      unsigned int destination = ctx->r[C_CTX_REGNO(store_reg)] + offset;
-
-	      if (destination < 4096)
-		{
-		  write (1, "Handled write to zero page\n", 27);
-		  ctx->epc += 4;
-		  return;
-		}
-	    }
-	  break;
-
-	case EXC_TLBL:
-	  fault_instruction = *((unsigned int *) (ctx->epc));
-	  if ((fault_instruction >> 26) == 0x23)
-	    {
-	      /* Load instruction, ensure it went to zero. */
-	      unsigned int load_reg = (fault_instruction >> 21) & 0x1f;
-	      int16_t offset = fault_instruction & 0xffff;
-	      unsigned int destination = ctx->r[C_CTX_REGNO(load_reg)] + offset;
-	      if (destination < 4096)
-		{
-		  write (1, "Handled read from zero page\n", 28);
-		  ctx->epc += 4;
-		  unsigned int rt = (fault_instruction >> 16) & 0x1f;
-		  ctx->r[rt] = 0;
-		  return;
-		}
-	    }
-	  break;
-
+      && (exception == EXC_TRAP))
+  {
+#ifdef __mips_micromips
+    unsigned int trap_insn = ((*(unsigned short *)ctx->epc & ~1) << 16
+			      | *(unsigned short *)((ctx->epc & ~1) + 2));
+    switch ((trap_insn >> 12) & 0xf)
+#else
+    unsigned int trap_insn = *(unsigned int *)ctx->epc;
+    switch ((trap_insn >> 6) & 0x3ff)
+#endif
+      {
+	case TRAPCODE1:
+	  secret_store = ctx->r[C_CTX_REGNO (12)];
+	  /* Trash the value */
+	  ctx->r[C_CTX_REGNO (12)] = 0;
+	  write(1, "Trap 1 done!\n", 13);
+	  num_traps += 1;
+	  ctx->epc += 4;
+	  return;
+	case TRAPCODE2:
+	  ctx->r[C_CTX_REGNO (12)] = secret_store;
+	  write(1, "Trap 2 done!\n", 13);
+	  num_traps += 1;
+	  ctx->epc += 4;
+	  return;
 	default:
 	  break;
-	}
-    }
+      }
+  }
 
   /* All other exceptions are passed to the default exception handler.  */
   __exception_handle (ctx, exception);
