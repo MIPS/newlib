5fb07e50f828d0c3467c0e1b9159688a8bddb3ba
Chao-ying Fu <cfu@wavecomp.com>
2025-12-30 14:03:28 -0800

    Fix struct stat to work with MIPS uhi code.

--- newlib/libc/include/sys/stat.h
+++ newlib/libc/include/sys/stat.h
@@ -38,6 +38,16 @@ struct	stat
   time_t	st_atime;
   time_t	st_mtime;
   time_t	st_ctime;
+#elif defined(__mips__)
+  time_t	st_atime;
+  long		st_spare1;
+  time_t	st_mtime;
+  long		st_spare2;
+  time_t	st_ctime;
+  long		st_spare3;
+  blksize_t	st_blksize;
+  blkcnt_t	st_blocks;
+  long		st_spare4[2];
 #else
   struct timespec st_atim;
   struct timespec st_mtim;
@@ -50,7 +60,7 @@ struct	stat
 #endif
 };
 
-#if !(defined(__svr4__) && !defined(__PPC__) && !defined(__sun__))
+#if !(defined(__svr4__) && !defined(__PPC__) && !defined(__sun__)) && !defined(__mips__)
 #define st_atime st_atim.tv_sec
 #define st_ctime st_ctim.tv_sec
 #define st_mtime st_mtim.tv_sec
